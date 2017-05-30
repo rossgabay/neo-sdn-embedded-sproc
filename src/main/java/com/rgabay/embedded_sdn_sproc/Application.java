4fa3aa46d105f57cb1db76957d97614539e68e4d
rossgabay <rossgabay@Ross-Gabays-MacBook-Pro.local>
2017-05-30 13:21:12 -0400

    example of external plugin dir usage for embedded sproc

--- src/main/java/com/rgabay/embedded_sdn_sproc/Application.java
+++ src/main/java/com/rgabay/embedded_sdn_sproc/Application.java
@@ -2,15 +2,16 @@
 package com.rgabay.embedded_sdn_sproc;
 
 import com.rgabay.embedded_sdn_sproc.domain.Person;
-import com.rgabay.embedded_sdn_sproc.procedure.PersonProc;
 import com.rgabay.embedded_sdn_sproc.repository.PersonRepository;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.kernel.impl.proc.Procedures;
-import org.neo4j.kernel.internal.GraphDatabaseAPI;
-import org.neo4j.ogm.config.Configuration;
+
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+
 import org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver;
 import org.neo4j.ogm.model.Result;
 import org.neo4j.ogm.service.Components;
+import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,7 @@ import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -32,68 +34,46 @@ import java.util.Collections;
 @EnableTransactionManagement
 public class Application {
 
-	private final static Logger log = LoggerFactory.getLogger(Application.class);
+    private static final String PLUGIN_DIR = "/Users/rossgabay/neo/neo4j-enterprise-3.1.2/plugins";
+    private final static Logger log = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
 
-	}
+    }
 
-	@Bean
+    @Bean
     @Transactional
-	CommandLineRunner demo(PersonRepository personRepository, Neo4jTemplate template) {
-		return args -> {
-
-            EmbeddedDriver embeddedDriver = (EmbeddedDriver) Components.driver();
-            GraphDatabaseService databaseService = embeddedDriver.getGraphDatabaseService();
-            ((GraphDatabaseAPI) databaseService).getDependencyResolver().resolveDependency(Procedures.class).registerProcedure(PersonProc.class);
+    CommandLineRunner demo(PersonRepository personRepository, Neo4jTemplate template, SessionFactory sessionFactory) {
+        return args -> {
 
             Person greg = new Person("Greg");
             greg.setArbitraryLongs(Arrays.asList(new Long(1), new Long(2)));
 
-			personRepository.save(greg);
+            personRepository.save(greg);
 
             personRepository.getPlist().forEach(System.out::println);
 
+            Session s =  sessionFactory.openSession();
 
-            System.out.println("now with sproc - annotated...");
-            personRepository.getPlistProc().forEach(System.out::println);
-
-            System.out.println("now with sproc - direct query...");
-            Result r = template.query("CALL com.rgabay.sprocnode(\"Person\")", Collections.EMPTY_MAP);
+            System.out.println("now with sproc...");
+            Result r = s.query("CALL com.rgabay.sprocnode(\"Person\")", Collections.EMPTY_MAP);
             r.forEach(System.out::println);
-
-		};
-	}
-
-    // embedded driver config example, to make the db non-permanent, remove .setURI call
-	@Bean
-    public Configuration configuration() {
-        Configuration config = new Configuration();
-        config
-                .driverConfiguration()
-                .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver");
-                //.setURI("file:///var/tmp/graph.db");
-        return config;
+        };
     }
 
-	// HTTP driver config example
+    @Bean
+    public SessionFactory sessionFactory() {
+        org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration();
+        Components.configure(configuration);
 
-	/*@Bean
-	public Configuration configuration() {
-		Configuration config = new Configuration();
-		config
-				.driverConfiguration()
-				.setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
-				.setURI("http://localhost:7474");
-		return config;
-	}*/
+        EmbeddedDriver driver = new EmbeddedDriver(graphDatabaseService());
+        Components.setDriver(driver);
 
-	@Bean
-	public SessionFactory sessionFactory() {
-		return new SessionFactory(configuration(), "com.rgabay.embedded_sdn_sproc");
+        configuration.driverConfiguration().setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver");
 
-	}
+        return new SessionFactory(configuration, "com.rgabay.embedded_sdn_sproc");
+    }
 
     @Bean
     public Neo4jTransactionManager transactionManager(SessionFactory sessionFactory) {
@@ -102,6 +82,17 @@ public class Application {
 
     @Bean
     public Neo4jTemplate neo4jTemplate(SessionFactory sessionFactory) {
+
         return new Neo4jTemplate(sessionFactory);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public GraphDatabaseService graphDatabaseService() {
+        GraphDatabaseService graphDatabaseService = new GraphDatabaseFactory()
+                .newEmbeddedDatabaseBuilder(new File("target/graph.db"))
+                .setConfig(GraphDatabaseSettings.plugin_dir, PLUGIN_DIR)
+                .newGraphDatabase();
+
+        return graphDatabaseService;
     }
 }
